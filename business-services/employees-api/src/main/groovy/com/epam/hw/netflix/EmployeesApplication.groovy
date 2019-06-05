041e2864240448010afabdc5214b395d21915fe5
Anton Sozonov <unboundnomad@gmail.com>
2019-06-05 14:11:05 +0400

    done without profile

--- business-services/employees-api/src/main/groovy/com/epam/hw/netflix/EmployeesApplication.groovy
+++ business-services/employees-api/src/main/groovy/com/epam/hw/netflix/EmployeesApplication.groovy
@@ -2,8 +2,10 @@ package com.epam.hw.netflix
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients
 class EmployeesApplication {
     static void main(String[] args) {
         SpringApplication.run(EmployeesApplication, args)
